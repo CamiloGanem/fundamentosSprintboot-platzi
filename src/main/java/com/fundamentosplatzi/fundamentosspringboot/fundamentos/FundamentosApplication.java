974b26fe72ff11bd7e6287d56186d1405fad1304
Camilo Ganem <camiloganemortega@gmail.com>
2021-11-16 09:52:16 -0500

    Clase Numero 25 de fundamentos Sprint boot

--- src/main/java/com/fundamentosplatzi/fundamentosspringboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/fundamentosspringboot/fundamentos/FundamentosApplication.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -48,6 +49,17 @@ public class FundamentosApplication implements CommandLineRunner {
 	public void run(String... args) {
 	//	EjemplosAnteriores();
 	saveUserInDataBase();
+		getInformationJPQLFromUser();
+	}
+
+	private void getInformationJPQLFromUser(){
+		LOGGER.info("Usuario con el metodo getInformationJPQLFromUser" +
+				userRepository.findByUserEmail("Daniela@gmail.com")
+						.orElseThrow(()-> new RuntimeException("No se encontro el usuario")));
+
+		userRepository.findAndSort("Cam", Sort.by("id").descending())
+				.stream()
+				.forEach(user -> LOGGER.info("Usuario con metodo sort" + user));
 	}
 
 	private void saveUserInDataBase(){
--- src/main/java/com/fundamentosplatzi/fundamentosspringboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/fundamentosspringboot/fundamentos/configuration/GeneralConfiguration.java
@@ -9,10 +9,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connections.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
@@ -24,6 +27,18 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String JdbcUrl;
+
+    @Value("${driver}")
+    private String Driver;
+
+    @Value("${username}")
+    private String UserName;
+
+    @Value("${password}")
+    private String Password;
+
     @Bean
     public MyBeanWithProperties funtion(){
         return new MyBeanWithPropertiesImplement(name, apellido);
@@ -32,10 +47,13 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdb:h2:men:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(Driver);
+        dataSourceBuilder.url(JdbcUrl);
+        dataSourceBuilder.username(UserName);
+        dataSourceBuilder.password(Password);
         return dataSourceBuilder.build();
     }
+
+
+
 }
